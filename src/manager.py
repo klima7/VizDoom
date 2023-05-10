3c87f37f052a3ff9a4233e49101d8b4cea766672
klima7 <github@mail.ukasz.com>
2023-05-10 13:41:07 +0200

    Update manager.py

--- src/manager.py
+++ src/manager.py
@@ -13,7 +13,7 @@ actions = [
         ]
 
 
-class MultiplayerRewardedGame(RewardedDoomGame):
+class MultiplayerDoomGame(RewardedDoomGame):
     
     def __init__(
         self,
@@ -30,8 +30,14 @@ class MultiplayerRewardedGame(RewardedDoomGame):
         self.bot_players = bot_players
         self.processes = []
         
+        self.game_config.setup_game(self)
+        self.host_player.setup_game(self)
+        self.add_game_args(f'-host {len(self.gui_players)+1}')
+        
     def _player_client(self, player):
-        game = self.__create_game(player)
+        game = RewardedDoomGame(player.rewards_config)
+        self.game_config.setup_game(game)
+        player.setup_game(game)
         game.add_game_args('-join 127.0.0.1')
         
         game.init()
@@ -40,8 +46,7 @@ class MultiplayerRewardedGame(RewardedDoomGame):
             while not game.is_episode_finished():
                 state = game.get_state()
                 game.make_action(choice(actions))
-                # print('step', player.name)
-                # game.advance_action()
+                print('step', player.name)
 
                 if game.is_player_dead():
                     game.respawn_player()
@@ -51,22 +56,19 @@ class MultiplayerRewardedGame(RewardedDoomGame):
 
         game.close()
         
-    def __create_game(self, player):
-        game = RewardedDoomGame(player.rewards_config)
-        self.game_config.setup_game(game)
-        player.setup_game(game)
-        return game
-        
-    def spin(self):
+    def init(self):
         for client_player in self.gui_players:
-            process = Process(target=self._player_client, args=(client_player,)).start()
+            process = Process(target=self._player_client, args=(client_player,))
+            process.start()
             self.processes.append(process)
-            
-        self.clear_game_args()
-        self.game_config.setup_game(self)
-        self.host_player.setup_game(self)
-        self.add_game_args(f'-host {len(self.gui_players)+1}')
-        self.init()
+        super().init()
+        
+    def close(self):
+        super().close()
+        for process in self.processes:
+            process.kill()
+            process.join()
+        self.processes = []
 
 
 if __name__ == '__main__':
@@ -79,9 +81,9 @@ if __name__ == '__main__':
         single_death_penalty=200
     )
     
-    config = GameConfig('cig.cfg', timeout=None)
+    config = GameConfig('cig.cfg', timeout=10)
     
-    host = AgentPlayer('klima7', rewards_config=rewards_config, window_visible=True)
+    host = AgentPlayer('klima7', rewards_config=rewards_config, window_visible=False)
     
     gui_players = [
         AgentPlayer('oponent1'),
@@ -95,15 +97,30 @@ if __name__ == '__main__':
         BotPlayer(),
     ]
     
-    game = MultiplayerRewardedGame(config, host, gui_players, bots, log_rewards=True)
-    game.spin()
+    game = MultiplayerDoomGame(config, host, gui_players, bots, log_rewards=True)
+    game.init()
     
-    for i in range(10):
+    for i in range(1):
         while not game.is_episode_finished():
             game.make_action(choice(actions))
-            # print('step host')
+            print('step host')
 
         game.new_episode()
         print('new_episode host')
         
+    game.close()
+    print('finish')
+    
+    
+    game.init()
+    
+    for i in range(1):
+        while not game.is_episode_finished():
+            game.make_action(choice(actions))
+            print('step host')
+
+        game.new_episode()
+        print('new_episode host')
+        
+    game.close()
     print('finish')
