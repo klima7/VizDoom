ffcd0a05c2926223123774a1509de3a48fdc8f59
klima7 <github@mail.ukasz.com>
2023-05-28 15:33:06 +0200

    Add fps_test.py

---
+++ src/fps_test.py
@@ -0,0 +1,29 @@
+from time import time
+import tqdm
+from setup import setup_multiplayer_game
+
+
+def test_fps(iterations):
+    game = setup_multiplayer_game()
+    game.init()
+    start = time()
+
+    for i in tqdm.trange(iterations, leave=False):
+        if game.is_episode_finished():
+            game.new_episode()
+        state = game.get_state()
+        game.make_action([True, False, False])
+
+    end = time()
+    t = end - start
+
+    print("Results:")
+    print("Iterations:", iterations)
+    print("time:", round(t, 3), "s")
+    print("fps: ", round(iterations / t, 2))
+
+    game.close()
+
+
+if __name__ == '__main__':
+    test_fps(10000)
