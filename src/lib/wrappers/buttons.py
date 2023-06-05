7d91bb98387a4d51f12bbf809aed5421c4f3260f
klima7 <github@mail.ukasz.com>
2023-06-05 20:25:22 +0200

    Cleanup buttons wrapper

--- src/lib/wrappers/buttons.py
+++ src/lib/wrappers/buttons.py
@@ -1,22 +1,20 @@
 class ModifyButtonsWrapper:
 
-    def __init__(self, game, available_buttons=(), delta_buttons=None):
+    def __init__(self, game, available_buttons=None, delta_buttons=None):
         self.game = game
-        self.__available_buttons = available_buttons
+        self.__available_buttons = available_buttons if available_buttons else []
         self.__delta_buttons = delta_buttons if delta_buttons else {}
 
-        self.__in_buttons = None
-        self.__mapping = None
+        self.__inside_buttons = None
+        self.__outside_inside_map = None
 
     def __getattr__(self, attr):
         return getattr(self.game, attr)
 
     def init(self):
         self.game.init()
-        self.__in_buttons = self.game.get_available_buttons()
-        self.__mapping = self.__create_buttons_mapping()
-        print(self.__mapping)
-        assert False
+        self.__inside_buttons = self.game.get_available_buttons()
+        self.__outside_inside_map = self.__create_outside_inside_map()
 
     def get_available_buttons(self):
         return tuple(button for button in self.game.get_available_buttons() if button in self.__available_buttons)
@@ -26,21 +24,21 @@ class ModifyButtonsWrapper:
 
     def make_action(self, action, *args, **kwargs):
         assert 0 <= action < len(self.__available_buttons), 'Illegal action'
-        new_action = self.__mapping[action]
-        self.game.make_action(new_action, *args, **kwargs)
+        inside_button = self.__outside_inside_map[action]
+        self.game.make_action(inside_button, *args, **kwargs)
 
-    def __create_buttons_mapping(self):
-        return {idx: self.__create_vector_for_button(button) for idx, button in enumerate(self.__available_buttons)}
+    def __create_outside_inside_map(self):
+        return {idx: self.__create_inside_action(button) for idx, button in enumerate(self.__available_buttons)}
 
-    def __create_vector_for_button(self, button):
-        in_button_idx = self.__in_buttons.index(button)
-        max_vector = self.__create_max_vector()
-        vector = [(val if idx == in_button_idx else 0) for idx, val in enumerate(max_vector)]
+    def __create_inside_action(self, outside_button):
+        inside_button_idx = self.__inside_buttons.index(outside_button)
+        inside_action = self.__create_max_inside_action()
+        vector = [(val if idx == inside_button_idx else 0) for idx, val in enumerate(inside_action)]
         return vector
 
-    def __create_max_vector(self):
-        max_vector = [1] * len(self.__in_buttons)
+    def __create_max_inside_action(self):
+        max_inside_action = [1] * len(self.__inside_buttons)
         for button, value in self.__delta_buttons.items():
-            idx = self.__in_buttons.index(button)
-            max_vector[idx] = value
-        return max_vector
+            inside_button_idx = self.__inside_buttons.index(button)
+            max_inside_action[inside_button_idx] = value
+        return max_inside_action
